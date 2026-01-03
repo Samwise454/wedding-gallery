c27c5c9bf3274d18f6bbcad448db42fe64d7db78
Sam C <chuquest.work@gmail.com>
2026-01-03 13:57:12 +0100

    Minor tweak to home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,7 +17,8 @@ const Home = () => {
     }, [visitor]);
 
     const handleInput = (e) => {
-        let username = e.target.value.charAt(0).toUpperCase() + e.target.value.slice(1);
+        let username = e.target.value.toLowerCase();
+        username = e.target.value.charAt(0).toUpperCase() + e.target.value.slice(1);
         setVisitorInput(username);
     }
 
